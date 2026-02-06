9f148bfef459189f9c6aeaeb7fa89db13fb751f7
celestial04 <perso@katsuu04.dev>
2026-02-06 15:37:55 +0000

    Remove priority attribute from image for unoptimized loading

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           alt="Celeste's Discord profil picture."
           width={100}
           height={20}
-          priority
+          unoptimized
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
