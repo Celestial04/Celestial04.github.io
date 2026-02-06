6e1bdbdbd166463bdd7a6b1f7d0b46c3737c4942
celestial04 <perso@katsuu04.dev>
2026-02-06 21:04:57 +0000

    favicon and nothing else (no really)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Playpen_Sans } from "next/font/google";
 import "./globals.css";
 import { styleText } from "util";
 
@@ -13,6 +13,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const playPenSans = Playpen_Sans({
+  subsets: ["latin"]
+})
+
 export const metadata: Metadata = {
   title: "Celeste04.dev'",
   description:
@@ -21,6 +25,7 @@ export const metadata: Metadata = {
     title: "Celeste04.dev",
     description: "Celeste's portfolio website: a developper from France.",
   },
+  icons: ".."
 };
 
 export default function RootLayout({
@@ -31,11 +36,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[url(../public/bg.jpg)]`}
+        className={`${playPenSans.className} ${geistMono.variable} antialiased bg-[url(../public/bg.jpg)]`}
       >
-        <header className="sticky top-0 z-50 h-[2rem] mx-[15rem] justify-center backdrop-blur-sm drop-shadow-2xl bg-purple-500">
+        <header className="sticky top-0 z-50 h-[2rem] mx-[15rem] justify-center backdrop-blur-sm drop-shadow-2xl bg-purple-500 text-center ">
             <a
-              className="hover:bg-[#ffaadd] hover:text-[#0a0a0a] hover: transition duration-150 font-[24pt]"
+              className="text-xl text- bg-[#ffaadd] hover:text-[#0a0a0a] hover: transition duration-150"
               href="https://katsuu04.dev"
             >
               Main website
