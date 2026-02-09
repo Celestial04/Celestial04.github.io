0ddf25c1a50703f5ac39b7759a4a68c2787ab7d5
celestial04 <perso@katsuu04.dev>
2026-02-09 19:59:56 +0000

    analytics thing added

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               Hi, I&apos;m celeste! 👋
               </p>
             <p>
-              I&apos;m a 
+              I&apos;m a developper aged of 21yo who live in France
               </p>
           </div>
           <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
@@ -45,6 +45,7 @@ export default function Home() {
           </div>
         </div>
       </main>
+      <script defer src="https://analytics.celeste04.moe/script.js" data-website-id="b4c47dd4-9c88-4e8a-9034-6a01a251b2f6"></script>
     </div>
   );
 }
