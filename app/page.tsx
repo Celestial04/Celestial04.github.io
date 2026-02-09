cf952ffc61e311095aaf22ef276fa337716e761f
celestial04 <perso@katsuu04.dev>
2026-02-09 13:20:10 +0000

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           />
           
           <div className="flex flex-col sm:items-start sm:text-center">
-            <p className="text-2xl">hi!</p>
+            <p className="text-2xl">Hi! 👋</p>
             <p className="">
               aaaaaaaaaaaaaaaaaa
             </p>
