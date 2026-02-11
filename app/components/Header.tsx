cba7e21862c2e85d00073f325a6c4722b505ae9b
celestial04 <perso@katsuu04.dev>
2026-02-11 22:44:31 +0000

    more contents in the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,8 @@
 
 export default function Header() {
   return (
-    <header className="sticky flex top-0 z-50 h-14 backdrop-blur-lg my-auto shadow-md">
-
+    <header className="sticky flex px-1 top-0 z-50 h-14 backdrop-blur-lg  shadow-md">
+      <p className="content-center">Celeste.moe</p>
     </header>
   );
 }
