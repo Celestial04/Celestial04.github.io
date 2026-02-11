c0c192e6b2f72f21e6f94ba22800805db5ac1002
celestial04 <perso@katsuu04.dev>
2026-02-11 22:53:07 +0000

    fix: adjust padding in header component for improved layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header() {
   return (
-    <header className="sticky flex px-1 top-0 z-50 h-14 backdrop-blur-lg  shadow-md">
+    <header className="sticky flex px-4 top-0 z-50 h-14 backdrop-blur-lg shadow-md">
       <p className="content-center">Celeste.moe</p>
     </header>
   );
