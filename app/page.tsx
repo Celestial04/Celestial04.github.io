5ef170acbf8d29cedd6b444bbd9d1317bb28c905
celestial04 <perso@katsuu04.dev>
2026-02-12 10:37:47 +0000

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import pfp from '@/app/img/pfp.gif';
 
 export default async function Home() {
   return (
-      <main className="min-h-screen w-full py-16 px-16">
-        <div className="flex flex-col p-3.5 rounded-md bg-background md:w-1xl lg:w-2/4">
+      <main className="min-h-screen w-full px-4">
+        <div
+        className="flex flex-col p-3.5 rounded-md border-(foreground)border md:w-1xl lg:w-2/4"
+        >
           <Image
             src={pfp.src}
             alt="Celeste's Discord profile picture."
@@ -13,18 +15,19 @@ export default async function Home() {
             className="rounded-md m-auto"
             unoptimized
           />
-
           <div className="flex flex-col lg:text-center">
             <p className="text-2xl">
               Hi, I&apos;m celeste! 👋
               </p>
             <p>
-              I&apos;m a developer aged of 21yo who live in France
+              I&apos;m a developer aged of 21yo who live in France, 
               </p>
           </div>
           <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
             <a
-              className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:w-39.5"
+              className="flex h-12 w-full items-center justify-center gap-2
+              rounded-full bg-background px-5 text-foreground transition-colors
+              hover:bg-background/45 sm:w-39.5"
               href=""
               target="_blank"
               rel="noopener noreferrer"
@@ -33,7 +36,9 @@ export default async function Home() {
               button
             </a>
             <a
-              className="flex h-12 w-full items-center justify-center rounded-full border border-solid  px-5 transition-colors hover:border-transparent  dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:w-39.5"
+              className="flex h-12 w-full items-center justify-center rounded-full
+              border border-solid px-5 transition-colors hover:border-transparent
+              color-foreground border-foreground hover:bg-background sm:w-39.5"
               href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
               target="_blank"
               rel="noopener noreferrer"
