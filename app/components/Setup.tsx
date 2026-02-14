dd9d04324826a883ae2b4dd9e3a60ade7ae0c41c
celestial04 <perso@katsuu04.dev>
2026-02-14 15:43:21 +0000

    fix: refine Setup component styling by adjusting backdrop and border properties

--- app/components/Setup.tsx
+++ app/components/Setup.tsx
@@ -7,9 +7,9 @@ export default function Setup() {
     return(
       <div
       id="Setup"
-      className="flex mt-300 backdrop-blur-xl"
+      className="flex mt-300"
       >
-        <div className="border">
+        <div className="border-2 rounded-2xl w-full border-[#442833]">
         <h2 className="text-2xl">
           Setup
         </h2>
