3bc6356eff0d8acf25573c971ccce63a84e5de27
celestial04 <perso@katsuu04.dev>
2026-02-15 15:38:52 +0000

    updated PROJECTS div

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,12 +5,14 @@ export default function Projects() {
     return(
       <div
       id="Projects"
-      className="flex mt-300"
+      className="flex flex-col mt-300 border-2 rounded-2xl w-full border-(border-stats) p-4"
       >
-        <div className="border-2 rounded-2xl w-full border-(border-stats) p-4">
+        <div className="">
         <h2 className="text-2xl">
           Projects
         </h2>
+        </div>
+        <div className="backdrop-blur-md">
         <p>Here&apos;s all my projects:</p>
         </div>
       </div>
