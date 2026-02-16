d00533127aaf859358fcc29d7e34245f6ca50060
celestial04 <perso@katsuu04.dev>
2026-02-16 12:31:02 +0000

    ✨ feat(projects): enhance project description with links
    - add hyperlink to Dideet project name for direct access
    - include contact email link for collaboration opportunities

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -14,8 +14,10 @@ export default function Projects() {
         <div
         className="rounded-2xl backdrop-opacity-100 bg-opa"
         >
-          <h4 className="text-xl">Dideet:</h4>
-          <p>It&apos;s a social network that&apos;s I started to think about it since the last year</p>
+          <h4 className="text-xl"><a href="https://dideet.com">Dideet</a>:</h4>
+          <p>It&apos;s a social network that&apos;s I started to think about it since the last year but I research some people to help me to concretize this project, so if you are interested to build with me this project, you can
+          write me at{" "}
+           <a href="mailto:write@katsuu04.dev">write@katsuu04.dev</a>!</p> 
         </div>
       </div>
     </div>
