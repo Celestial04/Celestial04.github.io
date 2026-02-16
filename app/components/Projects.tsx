706c84dad1c474e61bac01805b57b9cc79b5b284
celestial04 <perso@katsuu04.dev>
2026-02-16 12:56:37 +0000

    💄 style(Projects): update class names for consistent styling
    - change border color class from border-(border-stats) to border-foreground
    - add padding and margin to project divs for uniform spacing
    
    ✨ feat(Projects): enhance project descriptions
    
    - update project links and descriptions for clarity
    - add detailed explanation for WebApper project functionality

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -4,14 +4,14 @@ export default function Projects() {
   return (
     <div
       id="Projects"
-      className="flex flex-col mt-300 border-2 rounded-2xl w-full border-(border-stats) backdrop-blur-md m-2 p-1 list-inside"
+      className="flex flex-col mt-300 border-2 rounded-2xl w-full border-foreground backdrop-blur-md m-2 p-1 list-inside"
     >
-      <div className="">
+      <div>
         <h2 className="text-2xl">Projects</h2>
       </div>
       <div className="backdrop-blur-md">
         <p>Here&apos;s all my projects:</p>
-        <div className="rounded-2xl bg-indigo-600/25">
+        <div className="rounded-2xl bg-indigo-600/25 p-4 mb-8">
           <h4 className="text-xl">
             <a href="https://dideet.com">Dideet</a>:
           </h4>
@@ -23,16 +23,22 @@ export default function Projects() {
             <a href="mailto:write@katsuu04.dev">write@katsuu04.dev</a>!
           </p>
         </div>
-                <div className="rounded-2xl bg-purple-300/25">
+                <div className="rounded-2xl bg-purple-300/25 p-4">
           <h4 className="text-xl">
-            <a href="https://dideet.com">Dideet</a>:
+            <a href="https://github.com/Celestial04/WebApperAndroid">
+            WebApper
+            </a>:
           </h4>
           <p>
-            It&apos;s a social network that&apos;s I started to think about it
-            since the last year but I research some people to help me to
-            concretize this project, so if you are interested to build with me
-            this project, you can write me at{" "}
-            <a href="mailto:write@katsuu04.dev">write@katsuu04.dev</a>!
+            It&apos;s a very simple-to-use (in my opinion) web browser app. Today&apos;s web browser
+            are very complicated to set. For example, for just change the default search engine
+            , you need to navigate into multiple menu, like this: 
+            <code>
+              tap to the settings icon {"> "}
+              search the search engine option, then tap on this setting {"> "} then tap again to 
+              finally change the search engine set by default
+              </code>...
+               {" "} So yeah, this app as proper goal: to simplify all menus of a web browser app.
           </p>
         </div>
       </div>
