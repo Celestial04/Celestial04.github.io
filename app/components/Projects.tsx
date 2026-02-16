fb2c70ae17adebe1dc4f680463ee8ba9d2cf6397
celestial04 <perso@katsuu04.dev>
2026-02-16 12:34:29 +0000

    💄 style(Projects): improve code readability in Projects component
    - reformat JSX for better readability and indentation consistency

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,13 +11,17 @@ export default function Projects() {
       </div>
       <div className="backdrop-blur-md">
         <p>Here&apos;s all my projects:</p>
-        <div
-        className="rounded-2xl backdrop-opacity-100 bg-opa"
-        >
-          <h4 className="text-xl"><a href="https://dideet.com">Dideet</a>:</h4>
-          <p>It&apos;s a social network that&apos;s I started to think about it since the last year but I research some people to help me to concretize this project, so if you are interested to build with me this project, you can
-          write me at{" "}
-           <a href="mailto:write@katsuu04.dev">write@katsuu04.dev</a>!</p> 
+        <div className="rounded-2xl backdrop-opacity-100 bg-opa">
+          <h4 className="text-xl">
+            <a href="https://dideet.com">Dideet</a>:
+          </h4>
+          <p>
+            It&apos;s a social network that&apos;s I started to think about it
+            since the last year but I research some people to help me to
+            concretize this project, so if you are interested to build with me
+            this project, you can write me at{" "}
+            <a href="mailto:write@katsuu04.dev">write@katsuu04.dev</a>!
+          </p>
         </div>
       </div>
     </div>
