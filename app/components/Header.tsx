ff64026c983f682b4eb105e363a133945412006f
Celestial04 <perso@katsuu04.dev>
2026-04-02 11:32:16 +0200

    💄 style(header): update layout and links in header component
    - add new links for "Setup" and "Projects" with hover effect
    - adjust flex properties for better alignment
    - remove period from alt text for consistency
    
    📦 build(next): update next.js version and dependencies
    
    - upgrade next.js from 16.1.6 to 16.2.2 for latest features and fixes
    - update optional dependencies related to next.js to match new version
    - update sharp to version 0.34.5 in optional dependencies

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,15 +5,22 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="sticky flex py-3 top-0 z-50  backdrop-blur-lg shadow-md">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto flex-row px-4">
         <a href="#main" className="flex-row flex gap-2 items-center">
           <Image
             src={icon.src}
             width={40}
             height={40}
-            alt="Website icon."
+            alt="Website icon"
           />
           <p className="text-3xl">Celeste04.moe</p>
+        </a>
+        <a href="#Setup" className="text-lg hover:underline">
+          Setup
+        </a>
+        -
+        <a href="#Projects" className="text-lg hover:underline">
+          Projects
         </a>
       </div>
     </header>
