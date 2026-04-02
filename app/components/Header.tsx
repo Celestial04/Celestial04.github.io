b2491a099bb01484ffe46f1c5019c996b4c6950b
Celestial04 <perso@katsuu04.dev>
2026-04-02 12:46:07 +0200

    💄 style(header): improve header layout and styling
    - add max-h-24 to header for consistent height
    - adjust flex and gap properties for better alignment
    - reorganize layout to support responsive design with max-md:flex-col
    - simplify link styling by removing redundant classes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,9 +4,13 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="sticky flex py-3 top-0 z-50  backdrop-blur-lg shadow-md">
-      <div className="container mx-auto flex-row px-4">
-        <a href="#main" className="flex-row flex gap-2 items-center">
+    <header className="sticky flex py-3 top-0 z-50 backdrop-blur-lg shadow-md max-h-24">
+      <div 
+      className="container mx-auto flex flex-row max-md:flex-col px-4 gap-2
+      items-center justify-between"
+      >
+        <div>
+        <a href="#main" className="flex-row flex">
           <Image
             src={icon.src}
             width={40}
@@ -15,13 +19,18 @@ export default function Header() {
           />
           <p className="text-3xl">Celeste04.moe</p>
         </a>
-        <a href="#Setup" className="text-lg hover:underline">
+        </div>
+        <div className="text-lg flex flex-row gap-4 justify-between">
+        <a href="#Setup">
           Setup
         </a>
-        -
-        <a href="#Projects" className="text-lg hover:underline">
+        <a href="#Projects">
           Projects
         </a>
+        <a href="#Me">
+          About Me
+        </a>
+      </div>
       </div>
     </header>
   );
