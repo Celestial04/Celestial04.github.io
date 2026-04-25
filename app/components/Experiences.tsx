4a95777a7eb4027eac0f569b6caade1a929bf697
celestial04 <perso@katsuu04.dev>
2026-04-25 02:47:30 +0200

    💄 style(components): improve layout and styling in Experiences component
    - remove unnecessary whitespace in className for better readability
    - adjust spacing in elements for consistent layout
    
    💄 style(globals): clean up unnecessary whitespace in global styles
    
    - remove extra lines in CSS for cleaner code structure

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -5,15 +5,14 @@ export default function Experiences() {
     <div
       id="Experiences"
       className="
-      
         flex scroll-m-24 flex-col mt-[67vh] border-2 rounded-2xl
-        w-full border-foreground backdrop-blur-md p-1 list-inside
+        w-full h-full border-foreground backdrop-blur-md p-1
         ">
       <div>
         <h2 className="text-2xl">Experiences</h2>
       </div>
-      <div className="backdrop-blur-md flex flex-col">
-        <div className="Bubble bg-amber-400/25 mb-8">
+      <div className="backdrop-blur-md flex flex-col my-2">
+        <div className="Bubble bg-amber-400/25 mb-2">
           <h4 className="text-xl">
             2026
           </h4>
